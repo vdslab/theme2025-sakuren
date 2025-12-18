136f0da274a8f975ea4656aae6264e3abfb5fcf3
mitchi <pg.saku0719@gmail.com>
2025-12-18 14:45:46 +0900

    テスト用画像作成

--- src/components/WordCloudDraw.tsx
+++ src/components/WordCloudDraw.tsx
@@ -96,9 +96,9 @@ const WordCloudDraw = ({
       onMouseLeave={() => onHover(null)}
     >
       <path
-        opacity={!selectedWord || findword ? 1 : 0.25}
+        opacity={!selectedWord || findword ? 0.75 : 0.25}
         d={pathGenerator(geoFeature) || ""}
-        fill="#99cc99"
+        fill="#d8f2d8ff"
         stroke="#333"
         strokeWidth={1}
         pointerEvents="visibleFill"
